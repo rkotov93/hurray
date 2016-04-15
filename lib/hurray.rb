a10c43b6a5533cd382dd3db6454f2ba0722609f6
Ruslan Kotov <rkotov93@gmail.com>
2016-04-15 13:14:26 +0200

    Add documentation comments

--- lib/hurray.rb
+++ lib/hurray.rb
@@ -1,8 +1,17 @@
+# ActiveRecord module for working with database arrays
 module Hurray
   extend ActiveSupport::Concern
 
   module ClassMethods
     # Get records ordered according to the ids order
+    #
+    # Example:
+    #   >> User.ordered_with([4,2,6])
+    #   => User::ActiveRecord_Relation
+    #   => [#<User id: 4 ...>, #<User id: 2 ...>, #<User id: 6 ...>]
+    #
+    # Arguments:
+    #   ids: (Array)
     def ordered_with(ids)
       order_clause = 'CASE id '
       ids.each_with_index do |id, index|
