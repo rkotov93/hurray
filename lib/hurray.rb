f1b300f273d36f4160b4bcb83d9c108ceeaabd9d
Ruslan Kotov <rkotov93@gmail.com>
2016-04-17 01:24:58 +0200

    Change ordered_with argument to key-value pair

--- lib/hurray.rb
+++ lib/hurray.rb
@@ -7,18 +7,18 @@ module Hurray
     #
     # Example:
     #   >> User.ordered_with([4,2,6])
-    #   => User::ActiveRecord_Relation
-    #   => [#<User id: 4 ...>, #<User id: 2 ...>, #<User id: 6 ...>]
+    #   => User::ActiveRecord_Relation [#<User id: 4 ...>, #<User id: 2 ...>, #<User id: 6 ...>]
     #
     # Arguments:
     #   ids: (Array)
-    def ordered_with(ids)
-      order_clause = 'CASE id '
-      ids.each_with_index do |id, index|
-        order_clause << sanitize_sql_array(['WHEN ? THEN ? ', id, index])
+    def ordered_with(pair = {})
+      column, array = pair.first
+      order_clause = "CASE #{column} "
+      array.each_with_index do |el, index|
+        order_clause << sanitize_sql_array(['WHEN ? THEN ? ', el, index])
       end
-      order_clause << sanitize_sql_array(['ELSE ? END', ids.length])
-      where(id: ids).order(order_clause)
+      order_clause << sanitize_sql_array(['ELSE ? END', array.length])
+      where(id: array).order(order_clause)
     end
   end
 end
