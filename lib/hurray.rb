8b37a011cb894d451fc3c4df1080bcc59faa06d4
Ruslan Kotov <rkotov93@gmail.com>
2016-04-18 00:12:58 +0200

    Fix and refactor ordered_with method

--- lib/hurray.rb
+++ lib/hurray.rb
@@ -11,14 +11,15 @@ module Hurray
     #
     # Arguments:
     #   ids: (Array)
-    def ordered_with(pair = {})
-      column, array = pair.first
+    def ordered_with(column, array, options = { provided: false, direction: :asc })
       order_clause = "CASE #{column} "
       array.each_with_index do |el, index|
         order_clause << sanitize_sql_array(['WHEN ? THEN ? ', el, index])
       end
-      order_clause << sanitize_sql_array(['ELSE ? END', array.length])
-      where(id: array).order(order_clause)
+      order_clause << sanitize_sql_array(['ELSE ? END ?', array.length, options[:direction].to_s])
+
+      return where(column => array).order(order_clause) if options[:provided]
+      order(order_clause)
     end
   end
 end
