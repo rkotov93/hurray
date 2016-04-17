36c6c94a4202646f251063f07f3e0a0407001a7b
Ruslan Kotov <rkotov93@gmail.com>
2016-04-18 00:22:56 +0200

    Edit documentation

--- lib/hurray.rb
+++ lib/hurray.rb
@@ -3,14 +3,25 @@ module Hurray
   extend ActiveSupport::Concern
 
   module ClassMethods
-    # Get records ordered according to the ids order
+    # Get records ordered according to the specified with array column order
     #
-    # Example:
-    #   >> User.ordered_with([4,2,6])
+    # Examples:
+    #   >> User.ordered_with(:id, [4,2,6])
+    #   => User::ActiveRecord_Relation [#<User id: 4 ...>, #<User id: 2 ...>, #<User id: 6 ...>, #<User id: 1 ...>, ...]
+    #
+    #   >> User.ordered_with(:id, [4,2,6], provided: true)
     #   => User::ActiveRecord_Relation [#<User id: 4 ...>, #<User id: 2 ...>, #<User id: 6 ...>]
     #
+    #   >> User.ordered_with(:name, %w(Nick Malvin John), provided: true)
+    #   => User::ActiveRecord_Relation [#<User name: Nick ...>, #<User name: Malvin ...>, #<User name: John ...>]
+    #
     # Arguments:
-    #   ids: (Array)
+    #   column:  (Key or String) # DB column
+    #   array:   (Array)         # Custom order
+    #   options: (Hash)
+    #     povided:   (Boolean)       # Get records satisfying array values
+    #     direction: (Key or String) # :asc or :desc
+
     def ordered_with(column, array, options = { provided: false, direction: :asc })
       order_clause = "CASE #{column} "
       array.each_with_index do |el, index|
