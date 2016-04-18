adb8c23d513671759acd026c291cd2bb59f267c7
Ruslan Kotov <rkotov93@gmail.com>
2016-04-18 02:17:37 +0200

    Fix ordered_with options

--- lib/hurray.rb
+++ lib/hurray.rb
@@ -9,10 +9,10 @@ module Hurray
     #   >> User.ordered_with(:id, [4,2,6])
     #   => User::ActiveRecord_Relation [#<User id: 4 ...>, #<User id: 2 ...>, #<User id: 6 ...>, #<User id: 1 ...>, ...]
     #
-    #   >> User.ordered_with(:id, [4,2,6], provided: true)
+    #   >> User.ordered_with(:id, [4,2,6], provided_only: true)
     #   => User::ActiveRecord_Relation [#<User id: 4 ...>, #<User id: 2 ...>, #<User id: 6 ...>]
     #
-    #   >> User.ordered_with(:name, %w(Nick Malvin John), provided: true)
+    #   >> User.ordered_with(:name, %w(Nick Malvin John), provided_only: true)
     #   => User::ActiveRecord_Relation [#<User name: Nick ...>, #<User name: Malvin ...>, #<User name: John ...>]
     #
     # Arguments:
@@ -22,14 +22,18 @@ module Hurray
     #     povided:   (Boolean)       # Get records satisfying array values
     #     direction: (Key or String) # :asc or :desc
 
-    def ordered_with(column, array, options = { provided: false, direction: :asc })
+    def ordered_with(column, array, options = {})
+      direction = options[:direction].to_s.upcase
+      direction = %w(ASC DESC).include?(direction) ? direction : 'ASC'
+      provided_only = options[:provided_only].nil? ? false : options[:provided_only]
+
       order_clause = "CASE #{column} "
       array.each_with_index do |el, index|
         order_clause << sanitize_sql_array(['WHEN ? THEN ? ', el, index])
       end
-      order_clause << sanitize_sql_array(['ELSE ? END ?', array.length, options[:direction].to_s])
+      order_clause << sanitize_sql_array(['ELSE ? END ', array.length]) << direction
 
-      return where(column => array).order(order_clause) if options[:provided]
+      return where(column => array).order(order_clause) if provided_only
       order(order_clause)
     end
   end
